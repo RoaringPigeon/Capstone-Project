78875a4e9a041f8f460cd6c899afba17025b2ecd
Andrew Woodward <roaringpigeon@gmail.com>
2020-02-09 21:01:21 -0500

    Changed from module to package and updated functions listed in description.
    Login page and forms now function properly.
    Register page and forms now function properly.
    DB is created and can store users(username, email, hashed password) and classrooms(room number, availability, booked).

--- crb/forms.py
+++ crb/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from crb.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators = [DataRequired(), Length(min=2, max=20)])
@@ -8,6 +9,15 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators = [DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators = [DataRequired(), EqualTo('password') ])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username already exists. Please choose a different username.')
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('An account has already been created with this Email.')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators = [DataRequired(), Email()])
