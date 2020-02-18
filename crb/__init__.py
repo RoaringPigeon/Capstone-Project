3bfd40ce41b815c212995f38ac7f40ae868e8c9d
Jinyoung An <jinyoungan85@gmail.com>
2020-02-18 02:41:11 -0500

    Update: Confirmation Email
    Now Flask-Mail send a confirmation email to new user's email address which requires to click attached link to validate the email. If user clicks the link, the value of 'email_confirmed' column changes to True.

--- crb/__init__.py
+++ crb/__init__.py
@@ -6,6 +6,7 @@ from flask_login import LoginManager
 app = Flask(__name__, static_url_path='/static')
 app.config['SECRET_KEY'] = '73982ac8l3l2hs7blh43l29bliha82914'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
--- crb/models.py
+++ crb/models.py
@@ -1,5 +1,7 @@
 from crb import db, login_manager
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -10,15 +12,16 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    email_confirmed = db.Column(db.Boolean, default=False)  # for confirmation email, default value is False.
 
-    def __repr__(self):
+    def __repr__(self):  # method for debugging/test
         return f"User('{self.username}', '{self.email}')"
 
 class ClassRoom(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     roomNumber = db.Column(db.String(3), unique=True, nullable=False)
     availability = db.Column(db.Boolean, unique=False, default=True)
-    booked = db.Column(db.Boolean, unique=False, default=False)
+    # booked = db.Column(db.Boolean, unique=False, default=False)
 
-    def __repr__(self):
+    def __repr__(self):  # method for debugging/test
         return f"ClassRoom('{self.roomNumber}', '{self.availability}', '{self.booked}')"
